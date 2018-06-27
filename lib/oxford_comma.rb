f9da6c7946e945b40aebc5c1862058097cbb6ee8
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-27 21:52:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,9 @@ def oxford_comma(array)
     array.join(",")
   elsif array.length ==2
     array.join("and")
-  elsif
+  elsif array.length ==3
+    blah = array.pop()
+    new_array=array.join(",")
   
   else
     
